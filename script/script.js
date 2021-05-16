4f04e94696c31f074c23c496ef558f0f41f7ee4d
Arthur CS <arthur.cs147@gmail.com>
2021-05-16 13:46:03 -0300

    Changed the way to select the inputs, selecting one by one, separeted in functions and added regex to validate the email input.

--- script/script.js
+++ script/script.js
@@ -1,41 +1,59 @@
-// VEIFY IF THE FORMS WAS SUBMITTED
-document.querySelector('form').addEventListener('submit', (event)=>{
-    console.log('enviar o formulario');
+const firstname = document.querySelector('#firstname')
+const lastname = document.querySelector('#lastname')
+const email = document.querySelector('#email')
+const pwd = document.querySelector('#password')
 
-    // I CAN'T SEND THE FORMS
-    event.preventDefault()
+document.querySelector('#form').addEventListener('submit', e => {
+    e.preventDefault();
+
+    checkInputs();
 })
 
-const inputs = document.querySelectorAll('input')
-const btn = document.querySelector('.btn')
-const error = document.querySelectorAll('.error')
-const emailInput = document.querySelector('.email')
+function checkInputs() {
+    // TRIM -> THE INPUT CANNOT BE FILL WITH SPACE
+    const firstnameValue = firstname.value.trim();
+    const lastnameValue = lastname.value.trim();
+    const emailValue = email.value.trim();
+    const passwordValue = pwd.value.trim();
 
-btn.onclick = () => {
-    if (inputs.values === undefined) {
+    if (firstnameValue === '')
+        inputError(firstname, 'First Name cannot be empty');
+    else
+        inputSuccess(firstname);
 
-    }else{
-        for (let i = 0; i < error.length; i++) {
-            if(inputs[i].value !== ''){
-                error[i].classList.add('invisible')
-                inputs[i].classList.remove('border-error')
-                continue
-            }else{
-                error[i].classList.remove('invisible')
-                inputs[i].classList.add('border-error')
-            }
-        }
-        emailInput.innerHTML = 'Email cannot be empty'
+    if (lastnameValue === '')
+        inputError(lastname, 'Last Name cannot be empty');
+    else
+        inputSuccess(lastname);
+
+    if (emailValue === '') {
+        inputError(email, 'Email cannot be empty');
+    } else if (!checkEmail(emailValue)) {
+        inputError(email, 'Looks Like this is not an email');
+    } else {
+        inputSuccess(email);
     }
+
+    if (passwordValue === '')
+        inputError(pwd, 'Password cannot be empty');
+    else
+        inputSuccess(pwd);
 }
 
+function inputError(input, message) {
+    const formControl = input.parentElement
+    const small = formControl.querySelector('small')
 
+    formControl.classList.add('error')
+    small.innerText = message
+}
 
+function inputSuccess(input) {
+    const formControl = input.parentElement
 
+    formControl.classList.remove('error')
+}
 
-
-
-
-
-
-
+function checkEmail(email) {
+    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+}
